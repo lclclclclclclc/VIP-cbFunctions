52595367ace8f3be5388aa79477d10df474d2647
lclclclclclclc <egibson@hmc.edu>
2018-05-29 11:17:03 -0400

    edit of TO's AddSessionCBMin for CuedOutcome task

--- AddSession_CuedOutcome.m
+++ AddSession_CuedOutcome.m
@@ -4,35 +4,31 @@
 %easily overlooked. It is worth spending some time at least scrolling over
 %the function reference and reading function headers once in a while
 
-%%
 
-path_to_session_directory = '';
-CuedEventsFN = ''; % proto-TrialEvents struct supplied to MakeTrialEvents
+%%filesystem orientation
+sessionDir = 'VIP-Ai32-NNx-6/20180413a';
+CuedEventsFN = 'CuedEvents.mat'; % proto-TrialEvents struct supplied to MakeTrialEvents
 
-%% find all new cells
-% alternative usage e.g. for adding a new session
-cells = findallcells(path_to_session_directory);
-
-%cf findcells()
+dataDir = getpref('cellbase', 'datapath');
+fullDir = [dataDir '/' sessionDir];
+%% find new cells
+cells = findallcells(sessionDir);
 
 %extract subject and session name
 [subject, session] = cellid2tags(cells{1});
 
 %% Load TTLs (segment if needed)
 returndir = pwd;
-cd(path_to_session_directory);
-
-    if ~exist('behavEVENTS.mat', 2)
+cd(fullDir);
+    if ~exist('behavEVENTS.mat', 'file')
         disp('behavEVENTS.mat not found.  Generating with segmentBehavMulti1Hz.m');
         segmentBehavMulti1Hz; %make this a function  
     end
-
     load('behavEVENTS.mat', 'Events_*'); %Events_TTL and Events_TS
-
-
-    %% Load the input struct for TrialEvents
-
-    if ~exist(CuedEventsFN, 2)
+    
+%% Handle behavior (CuedOutcome) data
+    % Load the input struct for TrialEvents
+    if ~exist(CuedEventsFN, 'file')
         filePickerMsg = strcat('Locate the CuedEvents file for session ', session);
         [fn, path] = uigetfile('*.mat', filePickerMsg, 'MultiSelect', 'off')
         CuedEventsFN = strcat(fn, path); % redefine CuedEventsFN with ui location
@@ -40,20 +36,25 @@ cd(path_to_session_directory);
 
     load(CuedEventsFN, 'CuedEvents'); %struct named CuedEvents
 cd(returndir);
-%% Align to recording data and save trial events file
 
-%this will save a TrialEvents.mat in session folder
-% with THE ADDITIONAL FIELD ***TrialStart*** containing the trial start
-% timestamps from the recording system for each trial
+% Align to recording data and save trial events file
+    % this will save a TrialEvents.mat in session folder
+    % with THE ADDITIONAL FIELD ***TrialStart*** containing the trial start
+    % timestamps from the recording system for each trial
 
-MakeTrialEvents(cells{1}, CuedEvents, Events_TTL, Events_TS);
+MakeTrialEvents(cells{1}, CuedEvents, Events_TTL, Events_TS, 'nameTrialStart', 'TrialStartTimes');
 % okay to do with only cells{1} because all from same session
 
-%% Define EventsEpochs function
-% [events,epochs] = defineEventsEpochs_CuedOutcome; %for demo, no need to call it extra in general
+% Prealign spikes
+prealignSpikes(cells,'FUNdefineEventsEpochs',@defineEventsEpochs_CuedOutcome,'filetype','behav','events',[],'epochs',[],'writing_behavior','overwrite','ifsave',1);
 
-%% Prealign the spikes
-prealignSpikes(cells,'FUNdefineEventsEpochs',@defineEventsEpochs_CuedOutcome,'filetype','event','events',[],'epochs',[],'writing_behavior','overwrite','ifsave',1);
+%% Handle stim (Photostim_1Hz, Photostim_multi) data
+
+% Write StimEvents.mat
+MakeStimEvents_Photostims(fullDir);
+
+% Prealign spikes
+prealignSpikes(cells,'FUNdefineEventsEpochs',@defineEventsEpochs_Photostims,'filetype','stim','events',[],'epochs',[],'writing_behavior','overwrite','ifsave',1);
 
 
 %% Create the single trial PSTH arrays
@@ -73,7 +74,7 @@ prealignSpikes(cells,'FUNdefineEventsEpochs',@defineEventsEpochs_CuedOutcome,'fi
 % cellid = CELLIDLIST{1};
 % TE = loadcb(cellid,'TrialEvents');
 % SE = loadcb(cellid,'EVENTSPIKES');
-% 
+
 % %% plot/get psth functions
 % % viewcell2b
 % % apsth
@@ -107,12 +108,12 @@ prealignSpikes(cells,'FUNdefineEventsEpochs',@defineEventsEpochs_CuedOutcome,'fi
 % 
 % % useful figure export function
 % %writefigs(gcf,path_to_fig) %writes to pdf and will append if pdf exists
-% 
-% %% what Paul and I do
-% % calculate single trial psth and save for different alignments
-% % calculate trial average psth for conditions from there
-% % faster but lots of customized functions
-% 
+
+%% what Paul and I do
+% calculate single trial psth and save for different alignments
+% calculate trial average psth for conditions from there
+% faster but lots of customized functions
+
 % %% Add cells to Cell Base / Compute the analysis
 % addcell(cells); %will compute added analyses
 % %cf addnewcells() addnewsessions() 
